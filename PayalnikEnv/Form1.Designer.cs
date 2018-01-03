059240b24fb11f2cf40465025dc14d27ff8dc309
sp <zoneofatombomb@gmail.com>
2018-01-03 17:19:23 +0300

    prrrrrrrrrraaaaaaaaa

--- PayalnikEnv/Form1.Designer.cs
+++ PayalnikEnv/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.environment = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.environment)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(654, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 112);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ПУСК";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(161, 152);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(351, 179);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pictureBox1_LoadCompleted);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // environment
+            // 
+            this.environment.BackColor = System.Drawing.Color.White;
+            this.environment.Dock = System.Windows.Forms.DockStyle.Left;
+            this.environment.Location = new System.Drawing.Point(0, 0);
+            this.environment.Name = "environment";
+            this.environment.Size = new System.Drawing.Size(647, 525);
+            this.environment.TabIndex = 1;
+            this.environment.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 525);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.environment);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.environment)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox environment;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- PayalnikEnv/Form1.cs
+++ PayalnikEnv/Form1.cs
@@ -15,6 +15,27 @@ namespace PayalnikEnv
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = Properties.Resources._1198773;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Refresh();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Green, 3);
+            Graphics env = pictureBox1.CreateGraphics();
+            pictureBox1.Image = Properties.Resources.pahalniik30;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            
         }
     }
 }
